1d1575d4d00229c55192d1118cef0e647b6079b9
Aleksandr <tmacazuki@gmail.com>
2023-12-14 07:13:56 +0300

    edit

--- bot/handlers/user_cmd.py
+++ bot/handlers/user_cmd.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
 router = Router()
 
@@ -8,3 +8,7 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message):
     await message.answer(f"Приветик {message.from_user.first_name}")
+
+@router.message(Command("help"))
+async def help(message: Message):
+    await message.answer("Это помощь по боту)")
